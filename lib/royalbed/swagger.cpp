82452ce687bec498cbe3c589cb271e0d9c6be090
alekseev <alekseev@olimp.lan>
2021-06-10 09:16:25 +0300

    Fixed export swagger

--- lib/royalbed/swagger.cpp
+++ lib/royalbed/swagger.cpp
@@ -16,7 +16,7 @@ void swagger(Router& router, const cmrc::embedded_filesystem& fs, std::string_vi
     const auto file = fs.open(std::string(swaggerApiFilePath));
     const auto mimeType = std::string(*mimeTypeForFileName(swaggerApiFilePath));
 
-    router.use("/swagger", staticFiles(cmrc::royalbed::swagger::get_filesystem()));
+    router.use("/", staticFiles(cmrc::royalbed::swagger::get_filesystem()));
     router.get("/swagger/doc-api", [file, mimeType](const auto& session) {
         session->set_headers({
           {"Content-Length", std::to_string(file.size())},
